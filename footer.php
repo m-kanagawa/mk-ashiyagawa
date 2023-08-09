5f754b3c94a05690d4f9e30075da63df412a0b0c
kanagawa <k.masahiro1980@gmail.com>
2023-08-09 15:00:57 +0900

    訪問歯科ページのカスタム

--- footer.php
+++ footer.php
@@ -1,6 +1,14 @@
 </main>
 <footer class="footer">
   <p class="pageTop"><a href="#">page top</a></p>
+	<?php if( is_page('home_visits') ) : ?>
+		<div class="cta home-visits">
+			<div class="cta__inner">
+				<p class="text"><span>無料</span>訪問歯科検診の<br>お申し込みはお電話で</p>
+				<p class="tel mk-btn01"><a href="tel:0120-96-9975" style="padding: 10px 0;"><span class="tel-num">0120-96-9975</span></a></p>
+			</div>
+		</div>
+	<?php else:?>
 	<div class="floating_box">
 		<p class="tel sp-only"><a href="tel:0797-38-3155"><img src="<?php echo get_template_directory_uri(); ?>/lib/img/share/icon_sp_tel.png" alt="0797-38-3155"></a></p>
 		<ul>
@@ -8,6 +16,7 @@
 			<li class="js-ani delay1"><a href="<?php echo home_url(); ?>/access/"><img src="<?php echo get_template_directory_uri(); ?>/lib/img/share/icon_parking.svg" alt="アクセス案内"><span>アクセス案内</span></a></li>
 		</ul>
 	</div>
+	<?php endif; ?>
   <?php wp_nav_menu( array(
     'theme_location'=>'fnav',
     'container'       => 'div',
@@ -18,7 +27,7 @@
   <p class="copyright"><small>&copy; 2020 <a href="<?php echo esc_url(home_url()); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></small></p>
 </footer>
 <?php wp_footer(); ?>
-<?php 
+<?php
   global $template;
   $temp_name = basename($template); // パスの最後の名前（ファイル名）を取得
   echo '<!-- Template file currently in use:'.$temp_name.' -->'; // ファイル名の表示
--- lib/parts/parts-h1.php
+++ lib/parts/parts-h1.php
@@ -3,14 +3,17 @@
 		<?php if(is_post_type_archive('news') || is_singular('news')):?>
 			<?php if(is_post_type_archive('news')):?>
 				<h1 class="page_ttl"><?php the_archive_title(); ?></h1>
-				<?php else:?>
+			<?php else:?>
 				<h1 class="page_ttl"><?php the_title(); ?></h1>
 			<?php endif; ?>
-			<figure><img src="<?php echo get_template_directory_uri(); ?>/lib//img/page/page_img_news.png" alt="<?php the_title(); ?>"></figure>
+			<figure><img src="<?php echo get_template_directory_uri(); ?>/lib/img/page/page_img_news.png" alt="<?php the_title(); ?>"></figure>
+		<?php elseif( is_page('home_visits') ) : ?>
+			<h1 class="page_home_visits_ttl"><img src="<?php echo get_template_directory_uri(); ?>/lib/img/page/page_home_visits_lp.jpg" class="pc-only" alt="訪問歯科診療"><img src="<?php echo get_template_directory_uri(); ?>/lib/img/page/page_home_visits_lp_sp.jpg" class="sp-only" alt="訪問歯科診療"></h1>
 		<?php else:?>
 			<h1 class="page_ttl"><?php the_title(); ?></h1>
-		<figure><img src="<?php if(get_field('page_ttl_bg')): ?><?php the_field('page_ttl_bg'); ?><?php else:?><?php echo get_template_directory_uri(); ?>/lib//img/page/page_img.png<?php endif; ?>" alt="<?php the_title(); ?>"></figure>
+		<figure><img src="<?php if(get_field('page_ttl_bg')): ?><?php the_field('page_ttl_bg'); ?><?php else:?><?php echo get_template_directory_uri(); ?>/lib/img/page/page_img.png<?php endif; ?>" alt="<?php the_title(); ?>"></figure>
 		<?php endif; ?>
+		<?php if( !is_page('home_visits') ) : ?>
 		<div class="three_point">
 			<a href="<?php echo home_url(); ?>/access/">
 				<ul>
@@ -20,5 +23,6 @@
 				</ul>
 			</a>
 		</div>
+		<?php endif; ?>
 	</div>
 </section>
